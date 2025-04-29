09f2ff164b05e14da9c71bb755d2cb435350f65f
Andrei Navarych <anavarich29@gmail.com>
2025-04-29 23:05:45 +0300

    fix: zero on appointments

--- src/features/profile/components/my-appointments.tsx
+++ src/features/profile/components/my-appointments.tsx
@@ -18,7 +18,6 @@ export const MyAppointments = () => {
 		localStorage.setItem('appointments', JSON.stringify(updatedRecords));
 	};
 
-	console.log(records);
 	return (
 		<div className="text-[20px] flex items-start flex-col w-[1240px] h-fit bg-white px-[60px]  dark:bg-bgDark gap-[30px] shadow-lg rounded-[15px] text-center relative border-[3px] border-primary dark:border-primaryDark self-center pb-[45px] pt-[40px]">
 			<p className="dark:text-white text-[36px] self-start">Мои записи</p>
@@ -27,27 +26,26 @@ export const MyAppointments = () => {
 					Активные записи
 				</h2>
 				<div className="flex-wrap flex gap-[20px]">
-					{records.length &&
-						records
-							.filter((record: any) => new Date(record.date) > currentDate)
-							.map((record: any) => (
-								<div
-									key={record.id}
-									className="p-[15px] bg-primary dark:bg-primaryDark shadow-lg rounded-[15px] border-primary"
+					{records
+						.filter((record: any) => new Date(record.date) > currentDate)
+						.map((record: any) => (
+							<div
+								key={record.id}
+								className="p-[15px] bg-primary dark:bg-primaryDark shadow-lg rounded-[15px] border-primary"
+							>
+								<>{() => console.log(record)}</>
+								<p className="text-[20px] font-bold">{record.service}</p>
+								<p>Имя: {record.name}</p>
+								<p>Имя питомца: {record.petName}</p>
+								<p>Дата: {format(new Date(record.date), 'dd/MM/yyyy')}</p>
+								<button
+									onClick={() => deleteRecord(record.id)}
+									className="mt-[10px] text-red-500 hover:underline"
 								>
-									<>{() => console.log(record)}</>
-									<p className="text-[20px] font-bold">{record.service}</p>
-									<p>Имя: {record.name}</p>
-									<p>Имя питомца: {record.petName}</p>
-									<p>Дата: {format(new Date(record.date), 'dd/MM/yyyy')}</p>
-									<button
-										onClick={() => deleteRecord(record.id)}
-										className="mt-[10px] text-red-500 hover:underline"
-									>
-										Отменить запись
-									</button>
-								</div>
-							))}
+									Отменить запись
+								</button>
+							</div>
+						))}
 				</div>
 
 				<h2 className="text-[24px] font-bold text-black dark:text-white mt-[30px]">
